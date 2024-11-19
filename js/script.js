4d3318177922978483663e8ab493cecb0c064283
valentinaskrobacki <valentinaskrobacki@gmail.com>
2024-11-19 13:04:23 -0300

    actualizacion

--- js/script.js
+++ js/script.js
@@ -15,19 +15,6 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("canva");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-    }
-}
 
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
